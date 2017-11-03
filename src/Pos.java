82d990196ca5874a45d348e7d6ec9b48a3ea830b
issuser <issuser@ISSCR2911.stf.nus.edu.sg>
2017-11-03 15:38:26 +0800

    update pos

--- src/Pos.java
+++ src/Pos.java
@@ -2,6 +2,7 @@ import java.awt.print.Book;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class Pos {
 
@@ -14,8 +15,8 @@ public class Pos {
 		}
 	}
 	
-	public void displayOrderSummary() {
-		
+	public void displayOrderSummary(CustomerPurchase cp) { 
+	
 	}
 	
     public static void main(String[] args) throws IOException { 
@@ -62,6 +63,7 @@ public class Pos {
 	    		}	    	        
 	        	break;
 	        case 3: 
+	        	mypos.displayOrderSummary(cp);
 	        	break;
 	        default:
 	        	break;
