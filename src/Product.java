948055f7b08cbbb6e5c9a8ccf74a47885ff38f77
issuser <issuser@ISSCR2911.stf.nus.edu.sg>
2017-11-03 14:00:42 +0800

    Test update product

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,27 @@
 
 public class Product {
 
+	private String sku;
+	private int price;
+	
+	public Product(String sku, int price)
+	{
+		this.sku = sku;
+		this.price = price;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	} 
+	
 }
