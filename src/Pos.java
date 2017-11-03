8e112cd03072a8eae7734e3ddf27231aa3fe25a8
issuser <issuser@ISSCR2911.stf.nus.edu.sg>
2017-11-03 15:16:14 +0800

    Added main to pos

--- src/Pos.java
+++ src/Pos.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Pos {
 
@@ -6,11 +9,45 @@ public class Pos {
 		ProductCatalog pc = new ProductCatalog();
 		
 		for (Product p : pc.getProducts()) {
-			System.out.print(p.getSku() + ", price: " + p.getPrice());
+			System.out.println(p.getSku() + ", price: " + p.getPrice());
 		}
 	}
 	
 	public void displayOrderSummary() {
 		
 	}
+	
+    public static void main(String[] args) throws IOException { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = 1;
+        
+        Pos mypos = new Pos();        
+        while (input != 0)
+        {
+	        System.out.println("1: View Products");
+	        System.out.println("2: Add Product to Cart");
+	        System.out.println("3: Checkout");
+	        System.out.println("0: Quit");
+	        System.out.println("Enter Option:");	        
+	        try{
+	            input = Integer.parseInt(br.readLine());
+	        }catch(NumberFormatException nfe){
+	            System.err.println("Invalid Format!");
+	        }
+	        
+	        switch (input)
+	        {
+	        case 1:
+	        	mypos.viewProducts();
+	        	break;
+	        case 2:
+	        	break;
+	        case 3:
+	        	break;
+	        default:
+	        	break;
+	        }
+	        System.out.println("Goodbye");
+        }
+    } 
 }
